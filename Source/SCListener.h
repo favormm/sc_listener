e1b9fa1b2e1d40591f530dcc18466acda880e57f
Tomáš Znamenáček <tomas.znamenacek@gmail.com>
2010-10-13 09:01:36 +0200

    Commented source.

--- Source/SCListener.h
+++ Source/SCListener.h
@@ -11,12 +11,26 @@
 
 @property(readonly) BOOL listening;
 
+// You need to call this before you can read any data.
+// Safe to call even if the listener is already running.
 - (void) listen;
+
+// Safe to call even when not running.
 - (void) pause;
+
+// Safe to call even when not running.
 - (void) stop;
 
+// Average mike power, 0–1 including both ends.
+// Returns –1 if not currently running.
 - (float) averagePower;
+
+// Peak mike power, 0–1 including both ends.
+// Returns –1 if not currently running.
 - (float) peakPower;
+
+// Returns updated structure with the levels
+// for all channels. Not thread safe.
 - (AudioQueueLevelMeterState*) levels;
 
 @end
